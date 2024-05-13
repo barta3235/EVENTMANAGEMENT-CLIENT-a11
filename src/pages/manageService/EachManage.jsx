22c38067550f096fe90ee5fb3f5a144de35d6948
ahnaf daiyan <bartamitdaiyan@gmail.com>
2024-05-13 19:14:01 +0600

    manage user edit

--- src/pages/manageService/EachManage.jsx
+++ src/pages/manageService/EachManage.jsx
@@ -6,8 +6,8 @@ const EachManage = ({ manage }) => {
     const { servicearea, servicename, imageservice, name, email, price, userimage } = manage;
 
     return (
-        <div className="flex  rounded shadow-md overflow-hidden relative dark:bg-gray-50 dark:text-gray-800 px-2 py-4 md:gap-5 items-center">
-            <div className="self-stretch flex flex-col md:flex-row items-center px-3 flex-shrink-0 gap-3">
+        <div className="flex rounded shadow-md overflow-hidden relative dark:bg-gray-50 dark:text-gray-800 px-2 py-4 md:gap-7 items-center">
+            <div className="self-stretch flex flex-col lg:flex-row items-center px-3 flex-shrink-0 gap-3">
                 <button className="lg:tooltip" data-tip="Delete">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-8 w-8">
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"></path>
@@ -15,7 +15,7 @@ const EachManage = ({ manage }) => {
                 </button>
                 <button className="lg:tooltip" data-tip="Edit"><FaRegEdit className="text-[28px]"></FaRegEdit></button>
             </div>
-            <div className="flex flex-col md:flex-row gap-[50px] md:gap-[100px]">
+            <div className="flex flex-col lg:flex-row gap-[30px] lg:gap-[90px]">
                 <div>
                     <h1 className="font-semibold mb-[10px] text-[18px]"># Service Information</h1>
                     <h1 className="font-medium"><span className="font-medium">Category -</span> {servicename}</h1>
